b26fe12f3732024255266d7fa6eff3fbadee3b33
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-01-10 22:27:32 +0530

    Create eranana.js

---
+++ eranana.js
@@ -0,0 +1,9 @@
+if (navigator.onLine) {
+    // The user is online, so assume they are connected to some network
+    window.alert('Welcome to Our YouTube Multi-Viewer  | thank you For Using Our Service');
+    
+} else {
+    // The user is offline, handle accordingly
+    console.log('The user is offline');
+    // You can provide an alternative action here if needed
+}
